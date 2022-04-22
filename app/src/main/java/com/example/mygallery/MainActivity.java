c84993001169bb3a3fbd00400fa2506629e09030
NoySouliya <souliyatmv24@gmail.com>
2022-04-22 11:36:48 +0700

    add mainmenu

--- app/src/main/java/com/example/mygallery/MainActivity.java
+++ app/src/main/java/com/example/mygallery/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.mygallery;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button grid,list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        grid=findViewById(R.id.btGrid);
+        list=findViewById(R.id.btList);
+
+        grid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(getApplication(), GridView.class);
+                startActivity(myIntent);
+            }
+        });
+
+        list.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent2 = new Intent(getApplication(), ListView.class);
+                startActivity(myIntent2);
+            }
+        });
     }
 }
